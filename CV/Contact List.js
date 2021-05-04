bce3856df9615677e183bc68e0fd6fced9712fc8
unknown <gaiatamir2112@gmail.com>
2021-05-04 19:10:06 +0300

    7 ex

---
+++ CV/Contact List.js
@@ -0,0 +1,26 @@
+fetch('https://reqres.in/api/users?page=2').then(
+    response => response.json()
+).then(
+    responseJSON => createUserList(responseJSON.data)
+)
+    .catch(
+        err =>console.log(err)
+    );
+
+function createUserList(users){
+    console.log(users);
+    const cMain = document.querySelector("main");
+    for(let user of users){
+       const section= document.createElement('section') ;
+       section.innerHTML = `
+           <img src ="${user.avatar}" alt="Pofile Picture"/>
+           <div>
+             ${user.first_name} ${user.last_name}
+              <br>
+              <a href="mailto:${user.email}">Send email</a>
+           </div> ` ;
+        cMain.appendChild(section);
+    }
+}
+
+
